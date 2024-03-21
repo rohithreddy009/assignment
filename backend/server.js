5d652b6fc43deda078ba685ea8fe29f43c596132
Rohith Reddy <89250524+rohithreddy009@users.noreply.github.com>
2024-03-21 20:04:18 +0530

    bugfixes

--- backend/server.js
+++ backend/server.js
@@ -1,21 +1,17 @@
 require('dotenv').config();
 const express = require('express');
+const { PrismaClient } = require('@prisma/client');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const { PrismaClient } = require('@prisma/client');
 
 const prisma = new PrismaClient();
 const app = express();
-const port = process.env.PORT || 3001;
 
 app.use(cors());
 app.use(bodyParser.json());
 
-app.get('/', async(req, res) => {
-    res.send('Hello World!');
-  });
+const port = process.env.PORT || 3001;
 
-// POST endpoint for submitting code snippets
 app.post('/submit', async (req, res) => {
   const { username, codeLanguage, stdin, sourceCode } = req.body;
   try {
@@ -33,18 +29,9 @@ app.post('/submit', async (req, res) => {
   }
 });
 
-// GET endpoint to fetch all submissions
 app.get('/submissions', async (req, res) => {
   try {
-    const snippets = await prisma.snippet.findMany({
-      select: {
-        username: true,
-        codeLanguage: true,
-        stdin: true,
-        sourceCode: true,
-        createdAt: true,
-      },
-    });
+    const snippets = await prisma.snippet.findMany();
     res.json(snippets);
   } catch (error) {
     res.status(500).send({ message: "Error fetching snippets", error: error.message });
